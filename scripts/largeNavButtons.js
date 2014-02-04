0dcfb7bbff14ac665c3ae84b6f843fc5ddc126fd
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-04 00:16:39 -0800

    Added script to modify divs on hover

--- scripts/largeNavButtons.js
+++ scripts/largeNavButtons.js
@@ -1,3 +1,13 @@
 largeNavButtons = {
-  init: function() {}
+  lastBackgroundColor: null,
+  init: function() {
+    $(".large-nav-button").hover(largeNavButtons.hoverOn, largeNavButtons.hoverOff)
+  },
+  hoverOn: function() {
+    largeNavButtons.lastBackgroundColor = $(this).css("background")
+    $(this).css("background", "blue")
+  },
+  hoverOff: function() {
+    $(this).css("background", largeNavButtons.lastBackgroundColor)
+  }
 }
